73d70dba4b26c3595a3a6e64dd8f26d52a509d9e
Yichao Wu <wuyichao71@126.com>
2023-10-19 00:06:31 +0900

    before starting pa3

--- tests/klib-tests/tests/test_sprintf.c
+++ tests/klib-tests/tests/test_sprintf.c
@@ -71,60 +71,85 @@ int main()
   for (int i = 0; i < sizeof(data) / sizeof(int); i++)
   {
     sprintf(buf, "%d %s %s %d\n", data[i], "is", "always", data[i]);
+    printf(buf, "%d %s %s %d\n", data[i], "is", "always", data[i]);
     check(strcmp(buf, out[i]) == 0);
 
     sprintf(buf, "%hhd %s %s %hhd\n", data[i], "is", "always", data[i]);
+    printf(buf, "%hhd %s %s %hhd\n", data[i], "is", "always", data[i]);
     check(strcmp(buf, out_hh[i]) == 0);
 
     sprintf(buf, "%hd %s %s %hd\n", data[i], "is", "always", data[i]);
+    printf(buf, "%hd %s %s %hd\n", data[i], "is", "always", data[i]);
     check(strcmp(buf, out_h[i]) == 0);
 
     sprintf(buf, "%ld %s %s %ld\n", data[i], "is", "always", data[i]);
+    printf(buf, "%ld %s %s %ld\n", data[i], "is", "always", data[i]);
     check(strcmp(buf, out_l[i]) == 0);
 
     sprintf(buf, "%lld %s %s %lld\n", data[i], "is", "always", data[i]);
+    printf(buf, "%lld %s %s %lld\n", data[i], "is", "always", data[i]);
     check(strcmp(buf, out_ll[i]) == 0);
   }
 
   sprintf(buf, "%s\n", "Hello, world!");
+  printf(buf, "%s\n", "Hello, world!");
   check(strcmp(buf, "Hello, world!\n") == 0);
 
   sprintf(buf, "%10s\n", "Hello, world!");
+  printf(buf, "%10s\n", "Hello, world!");
   check(strcmp(buf, "Hello, world!\n") == 0);
 
   sprintf(buf, "%20s\n", "Hello, world!");
+  printf(buf, "%20s\n", "Hello, world!");
   check(strcmp(buf, "       Hello, world!\n") == 0);
 
   sprintf(buf, "%-20s\n", "Hello, world!");
+  printf(buf, "%-20s\n", "Hello, world!");
   check(strcmp(buf, "Hello, world!       \n") == 0);
 
   sprintf(buf, "%20.15s\n", "Hello, world!");
+  printf(buf, "%20.15s\n", "Hello, world!");
   check(strcmp(buf, "       Hello, world!\n") == 0);
 
   sprintf(buf, "%20.10s\n", "Hello, world!");
+  printf(buf, "%20.10s\n", "Hello, world!");
   check(strcmp(buf, "          Hello, wor\n") == 0);
 
   sprintf(buf, "%-20.10s\n", "Hello, world!");
+  printf(buf, "%-20.10s\n", "Hello, world!");
   check(strcmp(buf, "Hello, wor          \n") == 0);
 
   sprintf(buf, "%-20.30s\n", "Hello, world!");
+  printf(buf, "%-20.30s\n", "Hello, world!");
   check(strcmp(buf, "Hello, world!       \n") == 0);
 
   sprintf(buf, "%20.0s\n", "Hello, world!");
+  printf(buf, "%20.0s\n", "Hello, world!");
   check(strcmp(buf, "                    \n") == 0);
 
   int num = 123456789;
   sprintf(buf, "%4d\n", num);
+  printf(buf, "%4d\n", num);
   check(strcmp(buf, "123456789\n") == 0);
+
   sprintf(buf, "%15d\n", num);
+  printf(buf, "%15d\n", num);
   check(strcmp(buf, "      123456789\n") == 0);
+
   sprintf(buf, "%20.15d\n", num);
+  printf(buf, "%20.15d\n", num);
   check(strcmp(buf, "     000000123456789\n") == 0);
+
   sprintf(buf, "%-20.15d\n", num);
+  printf(buf, "%-20.15d\n", num);
   check(strcmp(buf, "000000123456789     \n") == 0);
+
   sprintf(buf, "%- 20.15d\n", num);
+  printf(buf, "%- 20.15d\n", num);
   check(strcmp(buf, " 000000123456789    \n") == 0);
+
   sprintf(buf, "%+20.15d\n", num);
+  printf(buf, "%+20.15d\n", num);
   check(strcmp(buf, "    +000000123456789\n") == 0);
   /* for(char *cp = buf; *cp; cp++) */
   /*   putch(*cp); */
